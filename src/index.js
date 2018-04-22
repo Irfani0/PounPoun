0cb7299f17c132b5da22226f1c6f3c6b9c284a95
Irfan <Irfani0>
2018-04-22 19:33:43 +0530

    lttrt

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as  Router, Route, Link} from "react-router-dom";
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById("root"));
+ReactDOM.render(<Router>< App /></Router>, document.getElementById("root"));
